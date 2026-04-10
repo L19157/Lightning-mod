6f4561f329e8767d2f8b633c497a9da467591373
L19157 <anikettembhekar23@gmail.com>
2026-04-10 19:28:18 +0530

    Update and rename react app to src/firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+apiKey: "PASTE",
+authDomain: "PASTE",
+projectId: "PASTE",
+storageBucket: "PASTE"
+messagingSenderId: "PASTE",
+appId: "PASTE"
+};
+
+initializeApp(firebaseConfig);
